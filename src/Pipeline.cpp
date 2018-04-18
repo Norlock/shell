8e3cde8a916cacd6afda541de95d376f909694f6
Joris <jwillems25@hotmail.com>
2018-04-18 02:09:06 +0200

    Need to fix multiple pipes

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -16,44 +16,52 @@ Pipeline::~Pipeline() {
  * Executes the commands on this pipeline.
  */
 void Pipeline::execute() {
-	const int commandsSize = commands.size();
 	int currentIndex = 0;
-	bool hasMorePipes = true;
 
-	if(commandsSize == 1) {
+	if(commands.size() == 1) {
 		commands[0]->execute();
 		return;
 	}
-	//while (hasMorePipes) {
-	// FIXME: Probably need to set up some pipe here?
 
-	int pid;
-	int p[2];
-	pipe(p);
+	while (hasMorePipes(currentIndex)) {
 
-	pid = fork();
-	std::cout << "pid: " << pid << std::endl;
-	hasMorePipes = false;
+		pid_t pid;
+		int fd[2];
 
-	if (pid == 0) {
-		// does the ... | more part
-		std::cout << "right pipe: " << commands[currentIndex + 1]->command << std::endl;
-		dup2( p[0], 0 ); // connect 0 to pipe
-		close(p[1]);
-		close(p[0]);
-		commands[currentIndex + 1]->execute();
-		exit(EXIT_SUCCESS);
-	} else if (0 < pid) {
-		// does the ls -li | ... part
-		std::cout << "left pipe: " << commands[0]->command << std::endl;
-		dup2( p[1], 1 ); // connect 1 to pipe
-		close(p[0]);
-		close(p[1]);
-		commands[currentIndex]->execute();
-	} else {
-		std::cerr << "Pipe failed" << std::endl;
+		pipe(fd);
+		pid = fork();
+
+		if (pid == 0) {
+			// does the ls -li | ... part
+			dup2( fd[1], 1 ); // connect 0 to pipe
+			close(fd[0]);
+			close(fd[1]);
+			commands[currentIndex]->execute();
+			exit(1);
+		} else if (0 < pid) {
+			// does the ... | more part
+			pid = fork();
+			if(pid == 0) { 
+				dup2( fd[0], 0); // connect 1 to pipe
+				close(fd[1]);
+				close(fd[0]);
+				commands[currentIndex + 1]->execute();
+				exit(1);
+				if(hasMorePipes(currentIndex)) continue;
+			}
+			else {
+				int status;
+				close(fd[0]);
+				close(fd[1]);
+				waitpid(pid, &status, 0);
+				currentIndex++; 
+			}
+		} else {
+			std::cerr << "Pipe failed" << std::endl;
+		}
 	}
+}
 
-	std::cout << "Current pid: " << getpid() << std::endl;
-	std::cout << "Komt hier ook" << std::endl;
+bool Pipeline::hasMorePipes(const int currentIndex) {
+	return currentIndex < commands.size() - 1; // currentIndex still lower than last index?
 }
